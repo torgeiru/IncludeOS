3e26d8dc8b53eec090c972c78405b9de3af05b06
torgeiru <torgeir.skovholt.pro@gmail.com>
2025-06-09 00:27:59 +0200

    Changed MUSL source. Patches embedded in new source.

--- deps/musl/default.nix
+++ deps/musl/default.nix
@@ -8,28 +8,28 @@ stdenv.mkDerivation rec {
   version = "1.2.5";
 
   src = fetchGit {
-    url = "git://git.musl-libc.org/musl";
-    rev = "0784374d561435f7c787a555aeab8ede699ed298";
+    url = "https://github.com/torgeiru/musl-includeos";
+    rev = "9166eea3221355808795fad178e1ad19427d21b4";
+    #url = "git://git.musl-libc.org/musl";
+    #rev = "0784374d561435f7c787a555aeab8ede699ed298";
   };
 
   enableParallelBuilding = true;
 
-  patches = [
-    ./patches/musl.patch
-    ./patches/endian.patch
-  ];
+  # patches = [
+  #   ./patches/musl.patch
+  #   ./patches/endian.patch
+  # ];
 
   passthru.linuxHeaders = linuxHeaders;
 
-  postUnpack = ''
-    echo "Replacing musl's syscall headers with IncludeOS syscalls"
-
-    cp ${./patches/includeos_syscalls.h} $sourceRoot/src/internal/includeos_syscalls.h
-    cp ${./patches/syscall.h} $sourceRoot/src/internal/syscall.h
-
-    rm $sourceRoot/arch/x86_64/syscall_arch.h
-    rm $sourceRoot/arch/i386/syscall_arch.h
-  '';
+  # postUnpack = ''
+  #   echo "Replacing musl's syscall headers with IncludeOS syscalls"
+  #   cp ${./patches/includeos_syscalls.h} $sourceRoot/src/internal/includeos_syscalls.h
+  #   cp ${./patches/syscall.h} $sourceRoot/src/internal/syscall.h
+  #   rm $sourceRoot/arch/x86_64/syscall_arch.h
+  #   rm $sourceRoot/arch/i386/syscall_arch.h
+  # '';
 
  configurePhase = ''
     echo "Configuring with musl's configure script"
