3aa551a18e775990694cfa0d7559399de1a91919
torgeiru <torgeir.skovholt.pro@gmail.com>
2025-06-23 01:01:19 +0200

    Fjernet system_lot stdout og set tid syscall support

--- src/musl/set_tid_address.cpp
+++ src/musl/set_tid_address.cpp
@@ -1,14 +1,7 @@
 #include "stub.hpp"
 
-static struct {
-  int tid = 1;
-  int* set_child_tid = nullptr;
-  int* clear_child_tid = nullptr;
-} __main_thread__;
-
 static long sys_set_tid_address(int* tidptr) {
-  __main_thread__.clear_child_tid = tidptr;
-  return __main_thread__.tid;
+  return -ENOSYS;
 }
 
 extern "C"
