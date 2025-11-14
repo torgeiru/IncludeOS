ef740355b4a9a46eafc14c2c37922e7e36e4298b
torgeiru <torgeir.skovholt.pro@gmail.com>
2025-11-14 16:01:56 +0100

    More stuff cleaned away

--- api/posix/file_fd.hpp
+++ api/posix/file_fd.hpp
@@ -4,28 +4,6 @@
 
 #include "fd.hpp"
 
-class File_FD : public FD {
-public:
-  explicit File_FD(const int id, const fs::Dirent ent, uint64_t offset = 0)
-      : FD(id), ent_ {ent}, offset_ {offset},
-        dir_pos{0}, dir_{nullptr}
-  {}
-
-  ssize_t read(void*, size_t) override;
-  ssize_t readv(const struct iovec*, int iovcnt) override;
-  int write(const void*, size_t) override;
-  int close() override;
-  off_t lseek(off_t, int) override;
-
-  long getdents(struct dirent *dirp, unsigned int count) override;
-
-  bool is_file() override { return true; }
-
-private:
-  fs::Dirent ent_;
-  uint64_t offset_;
-  size_t dir_pos;
-  fs::Dirvec_ptr dir_;
-};
+class File_FD : public FD {};
 
 #endif
