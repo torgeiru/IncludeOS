d128c8c83ea12839d843bbf996775b0d8e990bce
torgeiru <torgeir.skovholt.pro@gmail.com>
2026-07-25 11:37:12 +0200

    Removing the VirtioFS test

--- test/integration/fs/virtiofs/README.md
+++
@@ -1,1 +0,0 @@
-# VirtioFS integration test
--- test/integration/fs/virtiofs/shared_dir.sh
+++
@@ -1,5 +0,0 @@
-mkdir shared
-echo xxxxyyyyzzzzwwww > shared/seek_file.txt
-cp banana.txt shared
-# Copy banana.txt into this file
-touch shared/banana_copy.txt
